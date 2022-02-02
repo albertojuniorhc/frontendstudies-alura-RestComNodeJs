ed792ad08d95d6c36e6f502d911b9f5cd7755e22
Alberto Junior <albertojuniorhc@gmail.com>
2022-02-01 21:24:31 -0300

    React com NodeJs - Aula04 - Finalizada

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -7,7 +7,6 @@ module.exports = (app) => {
     
     app.post('/atendimentos', (req, res) => {
         const atendimento = req.body;
-        Atendimento.adiciona(atendimento);
-        res.send('Rota Atendimentos - POST')
+        Atendimento.adiciona(atendimento, res);
     });
 }
--- models/atendimentos.js
+++ models/atendimentos.js
@@ -2,20 +2,45 @@ const moment = require('moment')
 const conexao = require('../infraestrutura/conexao')
 
 class Atendimento {
-    adiciona(atendimento){
-        const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS');
-        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS');
-        const atendimentoDatado = {...atendimento, dataCriacao, data}
-        const sql = 'INSERT INTO Atendimentos SET ?'
+    adiciona(atendimento, res){
+        const dataCriacao = moment().format('YYYY-MM-DD HH:mm:ss');
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss');
 
+        const dataEhValida = moment(data).isSameOrAfter(dataCriacao);
+        const clienteEhValido = atendimento.cliente.length >= 5;
 
-        conexao.query(sql, atendimentoDatado, (erro, resultados) => {
-            if(erro){
-                console.log(erro);
-            } else {
-                console.log(resultados)
+        // console.log(dataEhValida, clienteEhValido)
+
+        const validacoes = [
+            {
+                nome: 'data',
+                mensagem: 'Data deve ser posterior a data atual',
+                valido: dataEhValida
+            },
+            {
+                nome: 'cliente',
+                mensagem: 'Nome do cliente deve ter mais de 5 caracteres.',
+                valido: clienteEhValido
             }
-        })
+        ];
+
+        const erros = validacoes.filter(campo => !campo.valido); //NAO ENTENDI A SINTAXE
+        const existemErros = erros.length;
+
+        if(existemErros){
+            res.status(400).json(erros)
+        } else {
+            const atendimentoDatado = {...atendimento, dataCriacao, data}
+            const sql = 'INSERT INTO Atendimentos SET ?'
+    
+            conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+                if(erro){
+                    res.status(400).json(erro);
+                } else {
+                    res.status(201).json(resultados);
+                }
+            });
+        }
     }
 }
 
