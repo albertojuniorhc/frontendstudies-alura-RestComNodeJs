682741e3e336a6178dd8e406e0fe9edbdd4d9fc2
Alberto Junior <albertojuniorhc@gmail.com>
2022-02-02 17:34:28 -0300

    Refactored Date function and conexaoFinal

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,16 +1,33 @@
 const moment = require('moment')
 const conexao = require('../infraestrutura/conexao')
+const formatoDataHora = 'YYYY-MM-DD HH:mm:ss'
+
+const padronizaDataHora = (dataHora) => {
+    return moment(dataHora, 'DD/MM/YYYY').format(formatoDataHora);
+}
+
+const geraDataHoraAtual = () => {
+    return moment().format(formatoDataHora);
+}
+
+const conexaoFinal = (sql, dados = null, res ) => {
+    console.log(dados, res)
+    conexao.query(sql, dados, (erro, resultados) => {
+        if(erro){
+            res.status(400).json(erro);
+        } else {
+            !dados ? res.status(200).json(resultados) :  res.status(201).json(dados);
+        }
+    });
+}
 
 class Atendimento {
     adiciona(atendimento, res){    
-    // adiciona({data, cliente, ...resAtendimento}, res){
-        const dataCriacao = moment().format('YYYY-MM-DD HH:mm:ss');
-        const dataMoment = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss'); // pegando a data enviada pelo client, e colocando no padrao do moment
+        const dataCriacao = geraDataHoraAtual();
+        const dataMoment = padronizaDataHora(atendimento.data);
 
         const dataEhValida = moment(dataMoment).isSameOrAfter(dataCriacao);
         const clienteEhValido = atendimento.cliente.length >= 5;
-
-        // console.log(dataEhValida, clienteEhValido)
 
         const validacoes = [
             {
@@ -33,40 +50,21 @@ class Atendimento {
         } else {
             const atendimentoDatado = {...atendimento, dataCriacao, data: dataMoment}
             const sql = 'INSERT INTO Atendimentos SET ?'
-    
-            conexao.query(sql, atendimentoDatado, (erro, resultados) => {
-                if(erro){
-                    res.status(400).json(erro);
-                } else {
-                    res.status(201).json(atendimento);
-                }
-            });
+
+            conexaoFinal(sql, atendimentoDatado, res);
         }
     }
 
     lista(res){
         const sql = 'SELECT * FROM Atendimentos';
 
-        conexao.query(sql, (erro, resultados) => {
-            if (erro){
-                res.status(400).json(erro);
-            } else {
-                res.status(200).json(resultados);
-            }
-        });
+        conexaoFinal(sql, null, res);
     }
 
     buscaPorId(id, res){
         const sql = `SELECT * FROM Atendimentos WHERE id=${id}`;
 
-        conexao.query(sql, (erro, resultados) => {
-            const atendimento = resultados[0]
-            if (erro){
-                res.status(400).json(erro);
-            } else {
-                res.status(200).json(atendimento);
-            }
-        });
+        conexaoFinal(sql, null, res);
     }
 
     altera(id, valores, res){
@@ -75,25 +73,13 @@ class Atendimento {
         }
         const sql = `UPDATE Atendimentos SET ? WHERE id=?`
 
-        conexao.query(sql, [valores, id], (erro, resultados) => {
-            if (erro){
-                res.status(400).json(erro);
-            } else {
-                res.status(200).json(valores);
-            }
-        });
+        conexaoFinal(sql, [valores, id], res);
     }
 
     delete(id, res){
         const sql = 'DELETE FROM Atendimentos WHERE id=?';
 
-        conexao.query(sql, id, (erro, resultados) => {
-            if (erro){
-                res.status(400).json(erro);
-            } else {
-                res.status(200).json({id});
-            }
-        });
+        conexaoFinal(sql, id, res);
     }
 }
 
