b141d9836e109cadf3c6abf329f50764015e5cd3
Alberto Junior <albertojuniorhc@gmail.com>
2022-02-06 15:35:10 -0300

    chore: fixes identention

--- config/customExpress.js
+++ config/customExpress.js
@@ -1,13 +1,10 @@
-const express = require('express');
-const consign = require('consign');
-const bodyParser = require('body-parser');
+const express = require("express");
+const consign = require("consign");
 
 module.exports = () => {
-    const app = express();
-    app.use(express.urlencoded({extended: true}));
-    app.use(express.json());
-    consign()
-        .include('controllers')
-        .into(app);
-    return app;
-}
+  const app = express();
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  consign().include("controllers").into(app);
+  return app;
+};
--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-const customExpress = require('./config/customExpress');
-const connection = require('./infrastructure/connection');
-const Tables = require('./infrastructure/tables')
+const customExpress = require("./config/customExpress");
+const connection = require("./infrastructure/connection");
+const Tables = require("./infrastructure/tables");
 
 connection.connect((error) => {
-    if(error){
-        console.log(error)
-    } else {
-        console.log('** Database Online! **')
-        
-        Tables.init(connection)
-        const app = customExpress();
-        const tcpPort = 3000;
-        app.listen(tcpPort, () => {
-            console.log(`WebServer running on ${tcpPort}`)
-        })
-        
-    }
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("** Database Online! **");
+
+    Tables.init(connection);
+    const app = customExpress();
+    const tcpPort = 3000;
+    app.listen(tcpPort, () => {
+      console.log(`WebServer running on ${tcpPort}`);
+    });
+  }
 });
